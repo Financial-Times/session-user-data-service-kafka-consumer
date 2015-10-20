e2cad37e08f9d434c5f529509e9c942a02fb3d54
Roland Vachter <roland.vachter@iquestgroup.com>
2015-10-20 12:25:56 +0300

    Updated to use POST for SUDS, and update/create in one step.

--- app/services/usersListsClient.server.services.js
+++ app/services/usersListsClient.server.services.js
@@ -10,36 +10,7 @@ const logger = require('../../config/logger');
 
 const loggerId = 'SERVER:' + config.processId;
 
-exports.addUser = (user) => {
-
-    return new Promise((fulfill, reject) => {
-
-        let stringBody = JSON.stringify(user);
-
-        fetch(config.userListsEndpoint + '/users', {
-            method: 'post',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'Content-Length': Buffer.byteLength(stringBody)
-
-            },
-            body: stringBody
-        })
-        .then(response => {
-            if (response.status >= 200 && response.status < 300) {
-                return response.json();
-            }
-            //We received a status we do not accept
-            logger.error('Unexpected response from users-lists API', response);
-            throw new Error(response.statusText);
-        })
-        .then(fulfill)
-        .catch(reject);
-    });
-};
-
-exports.editUser = (user) => {
+exports.updateOrCreateUser = (user) => {
 
     return new Promise((fulfill, reject) => {
 
@@ -49,8 +20,8 @@ exports.editUser = (user) => {
             lastName: user.lastName
         });
 
-        fetch(config.userListsEndpoint + '/users/' + user.uuid, {
-            method: 'patch',
+        fetch(config.userListsEndpoint + '/' + user.uuid, {
+            method: 'post',
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
@@ -62,10 +33,8 @@ exports.editUser = (user) => {
             if (response.status >= 200 && response.status < 300) {
                 return response.json();
             }
-            if (response.status !== 404) {
-                //We received a status we do not accept
-                logger.error('Unexpected response from users-lists API', response);
-            }
+            //We received a status we do not accept
+            logger.error('Unexpected response from SUDS API', response);
             throw new Error(response.statusText);
         })
         .then(fulfill)
--- app/utils/processUsers.server.utils.js
+++ app/utils/processUsers.server.utils.js
@@ -11,73 +11,53 @@ const loggerId = 'SERVER:' + config.processId;
 
 exports.process = (next) => {
 
-    let usersArray = [];
+	let usersArray = [];
 
-    emailsMapSingleton.forEach((userObject, uuid) => {
-        usersArray.push({
-            uuid: uuid,
-            email: userObject.email,
-            firstName: userObject.firstName,
-            lastName: userObject.lastName
-        });
-    });
+	emailsMapSingleton.forEach((userObject, uuid) => {
+		usersArray.push({
+			uuid: uuid,
+			email: userObject.email,
+			firstName: userObject.firstName,
+			lastName: userObject.lastName
+		});
+	});
 
-    async.eachLimit(usersArray, 10, updateOrCreateUser, next);
+	async.eachLimit(usersArray, 10, updateOrCreateUser, next);
 
 };
 
 
 function updateOrCreateUser(user, callback) {
 
-    logger.info(loggerId, 'Attempting to edit user', {user: user.uuid});
-    let userBeingSent = JSON.parse(JSON.stringify(user));
+	logger.info(loggerId, 'Attempting to edit user', {user: user.uuid});
+	let userBeingSent = JSON.parse(JSON.stringify(user));
 
-    // We attempt to edit the user
-    usersListsClient.editUser(user)
-        .then(() => {
-            logger.debug(loggerId, 'User updated', {user: user.uuid});
-            deleteFromMapIfUnchanged(userBeingSent);
-            callback();
-        })
-        .catch(updateError => {
-
-            if (updateError.message === 'Not Found') {
-
-                // If the User does not exist yet, we attempt to create it
-                logger.info(loggerId, 'Attempting to create user', {user: user.uuid});
-                return usersListsClient.addUser(user)
-                    .then(() => {
-                        logger.debug(loggerId, 'User created', {user: user.uuid});
-                        deleteFromMapIfUnchanged(userBeingSent);
-                        callback();
-                    })
-                    .catch(addError => {
-                        //We received an error from the user API.
-                        //We log the error and we will try again later
-                        logger.error(loggerId, addError);
-                        callback();
-                    });
-            }
-            else {
-                //We received an error from the user API different from 404.
-                //We log the error and we will try again later
-                logger.error(loggerId, updateError);
-                callback();
-            }
-        });
+	// We attempt to edit the user
+	usersListsClient.updateOrCreateUser(user)
+		.then(() => {
+			logger.debug(loggerId, 'User updated', {user: user.uuid});
+			deleteFromMapIfUnchanged(userBeingSent);
+			callback();
+		})
+		.catch(updateError => {
+			//We received an error from the user API different from 404.
+			//We log the error and we will try again later
+			logger.error(loggerId, updateError);
+			callback();
+		});
 
 }
 
 function deleteFromMapIfUnchanged (storedUser) {
-    let uuid = storedUser.uuid;
-    let userInMap = emailsMapSingleton.get(uuid);
-    if (storedUser.email === userInMap.email && storedUser.firstName === userInMap.firstName && storedUser.lastName === userInMap.lastName) {
-        emailsMapSingleton.delete(uuid);
-        logger.debug('User deleted from map', {user: uuid});
-    }
-    else {
-        logger.info('User kept in map', {user: uuid});
-    }
+	let uuid = storedUser.uuid;
+	let userInMap = emailsMapSingleton.get(uuid);
+	if (storedUser.email === userInMap.email && storedUser.firstName === userInMap.firstName && storedUser.lastName === userInMap.lastName) {
+		emailsMapSingleton.delete(uuid);
+		logger.debug('User deleted from map', {user: uuid});
+	}
+	else {
+		logger.info('User kept in map', {user: uuid});
+	}
 }
 
 exports.deleteFromMapIfUnchanged = deleteFromMapIfUnchanged;
